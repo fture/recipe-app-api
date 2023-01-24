1d42661c431665bcda3aec1c43318996431f88a1
unknown <hyonjun311@gmail.com>
2023-01-24 21:28:03 +0900

    v1.0.5

--- app/app/settings.py
+++ app/app/settings.py
@@ -70,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -80,7 +79,7 @@ DATABASES = {
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
     }
 
 }
